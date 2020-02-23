f6e2a449d8ce78923894c6e6962124320518ae91
Mya <55725523+mmyaaaaa@users.noreply.github.com>
2020-02-22 23:24:46 -0500

    Add files via upload

---
+++ summative/src/summative/Specificcourse.java
@@ -0,0 +1,33 @@
+package summative;
+
+
+import java.io.Serializable;
+
+//package summative;
+
+public class Specificcourse implements Serializable{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    // the following attributes are used when a course object is invoked for a
+    // specific course with a room (info for a student)
+    //float mark; 
+    int peopletakingit;
+    int roomnumber;
+    Teacher teacher;
+    int timeslot;
+    String name;
+
+// people in the specific class
+    //static int peopleinclass;
+    
+    public Specificcourse(String name) {
+        this.name = name;
+        //peopleinclass++;
+    }
+
+    public Specificcourse() { 
+    }
+
+}
